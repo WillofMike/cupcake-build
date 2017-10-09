de4ed51095b31f7c7e788edb7ad2415d7256db27
Mike <you@example.com>
2017-10-08 19:16:38 -0600

    pulling data from the cupcake file

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const express = require('express');
 
+const cupcakes = require('./data/cupcakes');
+
 const app = express();
 
 const port = 3000;
 
-app.set('view engine', 'hbs')
+app.set('view engine', 'hbs');
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-  res.render('index')
-})
+  res.render('index', {
+    title: 'cupcake reviews!',
+    cupcakes: cupcakes
+  });
+});
 
 app.post('/', (req,res) => {
   const post = req.body
 
   console.log(post);
   res.json(post)
-})
+});
 
 app.listen(port, () => {
-  console.log(`Got it connect on https://localhost:${port}`);
-})
+  console.log(`Connected on http://localhost:${port}`);
+});
---
+++ data/cupcakes.js
@@ -0,0 +1,22 @@
+const cupcakes = [
+  {
+    id:0,
+    name: 'Chocolate',
+    baker: 'Miranda',
+    averageRating: (3 + 4 + 2 + 4)/ 4
+  },
+  {
+    id:1,
+    name: 'Confetti Cake',
+    baker: 'Mike',
+    averageRating: (2 + 4 + 2 + 4)/ 4
+  },
+  {
+    id:2,
+    name: 'Frosted Cupcake',
+    baker: 'Rhonda',
+    averageRating: (5 + 5 + 5 + 4)/ 4
+  },
+]
+
+module.exports = cupcakes;
